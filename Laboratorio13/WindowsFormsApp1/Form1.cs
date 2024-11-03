acf5cb543ff9be22ff2d78e0a29cdf5cb774d0ef
Ricardo Pau <ricardo.pau@utp.ac.pa>
2024-11-03 12:08:24 -0500

    Primer commit en la rama main

---
+++ Laboratorio13/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string connectionString =@"Server=.\sqlexpress;Database=Northwind;TrustServerCertificate=true;Integrated Security=SSPI;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                // Abrir la conexión
+                conexion.Open();
+                MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+
+                // Limpieza del listbox
+                listBox1.Items.Clear();
+
+                // Query
+                string query = "SELECT ProductName FROM [dbo].[Products]";
+                SqlCommand command = new SqlCommand(query, conexion);
+
+                
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        
+                        listBox1.Items.Add(reader["ProductName"].ToString());
+                    }
+                }
+
+                
+                conexion.Close();
+                MessageBox.Show("Se cerró la conexión.");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
