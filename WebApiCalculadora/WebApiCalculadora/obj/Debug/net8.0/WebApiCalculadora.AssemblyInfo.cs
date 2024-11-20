a76a396d1e1cc8d12134bfcad66ec740617ee913
Ricardo Pau <ricardo.pau@utp.ac.pa>
2024-11-20 00:37:19 -0500

    subir proyecto WebApiCalculadora

--- WebApiCalculadora/WebApiCalculadora/obj/Debug/net8.0/WebApiCalculadora.AssemblyInfo.cs
+++ WebApiCalculadora/WebApiCalculadora/obj/Debug/net8.0/WebApiCalculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiCalculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ced327eb56ca3a0744122f112cb6fa2d7634a0cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiCalculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiCalculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
